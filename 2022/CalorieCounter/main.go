269c267a79ad565a9fe83ea1002c16ffd10a13c7
Jahred Danker <jahrede@gmail.com>
2023-06-13 20:56:00 -0500

    solved!

--- 2022/CalorieCounter/main.go
+++ 2022/CalorieCounter/main.go
@@ -3,44 +3,51 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 	"strconv"
 )
 
 func main() {
-	var elfNum int = 0;
-	var calories int =0;
+	var elfNum int = 0
+	var calories int = 0
+	elfList := make([]int, 0) // creates an empty slice
 	readFile, err := os.Open("input.txt")
 
 	if err != nil {
-        fmt.Println(err)
-    }
+		fmt.Println(err)
+	}
 
 	fileScanner := bufio.NewScanner(readFile)
-    fileScanner.Split(bufio.ScanLines)
+	fileScanner.Split(bufio.ScanLines)
 
 	for fileScanner.Scan() {
-		numStr := fileScanner.Text();
+		numStr := fileScanner.Text()
 		if len(numStr) != 0 {
 			num, err := strconv.Atoi(numStr)
 			if err != nil {
-				calories=calories+num;
+				fmt.Println(err)
+
+			} else {
+				calories = calories + num
+				elfList = append(elfList, calories)
 			}
-
-
 		} else {
+			fmt.Println("Elf Number: ", elfNum, "Calories: ", calories)
+			calories = 0
 			elfNum++
-
 		}
 
+	}
 
-        fmt.Println(fileScanner.Text())
-    }
+	max := elfList[0]
 
+	for _, num := range elfList {
+		if num > max {
+			max = num
+		}
+	}
+
+	fmt.Println("Most Calories:", max)
 	readFile.Close()
 
-
 }
-
-
