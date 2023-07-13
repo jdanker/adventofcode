48411da612e30fe0bffdda29498f08c1c9309363
Jahred Danker <jahrede@gmail.com>
2023-07-12 20:11:59 -0500

    started day 3

---
+++ 2022/day3/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	readFile, err := os.Open("input.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		
+	}
+
+
+}
