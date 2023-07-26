2917cda2b57b75e11b3b68383ebddb2197209aaf
Jahred Danker <jahrede@gmail.com>
2023-07-25 21:05:47 -0500

    initial structure thoughts

--- 2022/day3/main.go
+++ 2022/day3/main.go
@@ -17,8 +17,33 @@ func main() {
 	fileScanner.Split(bufio.ScanLines)
 
 	for fileScanner.Scan() {
+		line := fileScanner.Text()
+
+		// if input string is odd it cannot be a valid input
+		if len(line)%2 != 0 {
+			fmt.Println("Invalid input. Line is odd number of characters.")
+			return
+			
+		}
+
+		half := len(line) / 2
+		left := line[:half]
+		right := line[half:]
+
+
+
+
+
 		
 	}
 
 
+}
+
+func getPriority(r rune) int {
+	return 0
+}
+
+func compareString(s1, s2 string) rune {
+	return 'a'
 }
